b40bde948c7045f3c557d1faf41755993ed32404
tianjingle <2695062879@qq.com>
2020-09-23 19:37:20 +0800

    init

---
+++ src/main/java/com/scaffold/simple/admin/config/LiquibaseConfig.java
@@ -0,0 +1,28 @@
+package com.scaffold.simple.admin.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+/**
+ * @Author: tianjingle
+ * @Date: 2019/7/4 15:40
+ * @Discption: liquibase配置
+ */
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) throws Exception {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setShouldRun(false);
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog-master.xml");
+        liquibase.setContexts("development,test,production");
+        return liquibase;
+    }
+}
---
+++ src/main/java/com/scaffold/simple/admin/infrestraction/collecter/service/CollectionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.scaffold.simple.admin.infrestraction.collecter.service;
+
+import com.scaffold.simple.admin.infrestraction.collecter.CollectionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: tianjl
+ * @Date: 2020/9/23 18:22
+ * @Eamil: 2695062879@qq.com
+ */
+@Slf4j
+@Service
+public class CollectionServiceImpl implements CollectionService {
+}
